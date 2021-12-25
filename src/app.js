1a7c65548cb4fafc26775f00f66b73b444f4e1d4
caganert <caganegeertugrul@gmail.com>
2021-12-25 14:10:40 +0300

    createProject

---
+++ src/app.js
@@ -0,0 +1,37 @@
+console.log("Merhaba Kodlama.io")
+
+//JS type safe değildir
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+dolarDun = "9.20"
+{
+    let dolarDun = 9.10
+}   
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11
+
+console.log(euroDun)
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+
+console.log("</ul>")
+for(let i = 0;i<konutKredileri.lenght;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+{/* <ul>
+    <li>Konut Kredisi</li>
+    <li>Emlak Konut Kredisi</li>
+    <li>Kamu Konut Kredisi</li>
+</ul> */}
+
+console.log(konutKredileri)
+
